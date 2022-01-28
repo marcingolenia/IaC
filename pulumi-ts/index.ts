da15f5198b28b70ea0cf6c57f59c6986028618c3
Marcin <marcingolenia@gmail.com>
2022-01-29 00:29:51 +0100

    Fix postgres db

--- pulumi-ts/index.ts
+++ pulumi-ts/index.ts
@@ -1,7 +1,7 @@
 import * as az from "@pulumi/azure-native"
 
 const imageInDockerHub = "appsvcsample/python-helloworld:latest"
-const resourceGroup = new az.resources.ResourceGroup("appservice-docker-rg")
+const resourceGroup = new az.resources.ResourceGroup("pulik-rg")
 
 const plan = new az.web.AppServicePlan("pulik-plan", {
     resourceGroupName: resourceGroup.name,
@@ -13,11 +13,17 @@ const plan = new az.web.AppServicePlan("pulik-plan", {
     },
 })
 
+const userAssignedIdentity = new az.managedidentity.UserAssignedIdentity("pulik-identity", {
+    location: "westeurope",
+    resourceGroupName: resourceGroup.name,
+    resourceName: "pulik-identity"
+})
+
 const vault = new az.keyvault.Vault("vault", {
     location: "westeurope",
     properties: {
         accessPolicies: [{
-            objectId: "00000000-0000-0000-0000-000000000000",
+            objectId: userAssignedIdentity.principalId,
             permissions: {
                 secrets: [
                     "get",
@@ -28,16 +34,14 @@ const vault = new az.keyvault.Vault("vault", {
                     "recover"
                 ],
             },
-            tenantId: "00000000-0000-0000-0000-000000000000",
+            tenantId: userAssignedIdentity.tenantId,
         }],
         enabledForDeployment: true,
-        enabledForDiskEncryption: true,
-        enabledForTemplateDeployment: true,
         sku: {
             family: "A",
             name: "standard",
         },
-        tenantId: "00000000-0000-0000-0000-000000000000",
+        tenantId: userAssignedIdentity.tenantId,
     },
     resourceGroupName: resourceGroup.name,
     vaultName: "pulik-vault",
@@ -75,9 +79,16 @@ const pulikApp = new az.web.WebApp("pulik-app", {
 const server = new az.dbforpostgresql.Server("pulik-server", {
     location: "westeurope",
     properties: {
-        createMode: "PointInTimeRestore",
-        restorePointInTime: "2017-12-14T00:00:37.467Z",
-        sourceServerId: "/subscriptions/ffffffff-ffff-ffff-ffff-ffffffffffff/resourceGroups/SourceResourceGroup/providers/Microsoft.DBforPostgreSQL/servers/sourceserver",
+        administratorLogin: "pulik-admin",
+        administratorLoginPassword: "SztrongPa$$sword",
+        createMode: "Default",
+        minimalTlsVersion: "TLS1_2",
+        sslEnforcement: "Enabled",
+        storageProfile: {
+            backupRetentionDays: 7,
+            geoRedundantBackup: "Disabled",
+            storageMB: 128000,
+        },
     },
     resourceGroupName: resourceGroup.name,
     serverName: "pulik-server",
@@ -87,7 +98,7 @@ const server = new az.dbforpostgresql.Server("pulik-server", {
         name: "B_Gen5_2",
         tier: "Basic",
     }
-})
+});
 
 const database = new az.dbforpostgresql.Database("pulik-database", {
     charset: "UTF8",
@@ -95,10 +106,4 @@ const database = new az.dbforpostgresql.Database("pulik-database", {
     databaseName: "pulik-db",
     resourceGroupName: resourceGroup.name,
     serverName: server.name,
-})
-
-const userAssignedIdentity = new az.managedidentity.UserAssignedIdentity("pulik-identity", {
-    location: "westeurope",
-    resourceGroupName: resourceGroup.name,
-    resourceName: "pulik-identity"
 })
